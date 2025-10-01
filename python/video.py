f11b81b55cff258eff4b5ad8576b428f0d0bca3a
ZHAO <qinglingzhiqing@126.com>
2025-10-01 14:50:24 +0800

    读取本地摄像头；测试用

--- python/video.py
+++ python/video.py
@@ -2,7 +2,7 @@
 Author: ZHAO qinglingzhiqing@126.com
 Date: 2025-10-01 09:50:11
 LastEditors: ZHAO qinglingzhiqing@126.com
-LastEditTime: 2025-10-01 09:57:33
+LastEditTime: 2025-10-01 11:43:43
 FilePath: \python\video.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -10,8 +10,8 @@ import cv2
 from ultralytics import YOLO
 # Load a pretrained YOLO11n model
 model = YOLO("yolo11n.pt")
-cap = cv2.VideoCapture("http://192.168.1.111:81/stream")
-
+# cap = cv2.VideoCapture("http://192.168.1.111:81/stream")
+cap = cv2.VideoCapture(0)
 while True:
     success, img = cap.read()
     results=model(img)
